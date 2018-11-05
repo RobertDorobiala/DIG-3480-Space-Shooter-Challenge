22cc7fa4f9a839150c1197a25863cffb2d5c1b75
RobertDorobiala <42683580+RobertDorobiala@users.noreply.github.com>
2018-11-05 12:52:21 -0500

    Esacpe Key

--- Space Shooter/Assets/Scripts/GameController.cs
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -46,7 +46,10 @@ public class GameController : MonoBehaviour
             {
                 Application.LoadLevel(Application.loadedLevel);
             }
-        }   
+        }
+
+        if (Input.GetKey("escape"))
+            Application.Quit();
     }
 
     IEnumerator SpawnWaves ()
